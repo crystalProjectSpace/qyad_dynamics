a253e1ca6cde903ad00ca1b2518fc269ea088f4f
crystalProjectSpace <sevmailbox@yandex.ru>
2024-10-10 21:14:06 +0300

    first 'real' flying simulation

--- index.js
+++ index.js
@@ -22,23 +22,25 @@ function getADX(alpha) {
 	}
 }
 
-const KTH = 0.01
-const KPSI = 0.01
+const KTH = 4.5
+const KPSI = 4.5
 const g = 9.81
 
 function setControls(state, ctrl, prog) {
-	const deltaTh = prog.getTh(state[0])- state[2]
-	const deltaPsi = prog.getPsi(state[0]) - state[3]
+	const deltaTh = (prog.getTh(state[0])- state[6]) - state[4] * 3
+	const deltaPsi = prog.getPsi(state[0]) - state[7]
+	const Th = state[2]
 
 	const _rev1 = 0.5 + deltaTh * KTH + deltaPsi * KPSI
 	const _rev2 = 0.5 + deltaTh * KTH - deltaPsi * KPSI
 	const _rev3 = 0.5 - deltaTh * KTH + deltaPsi * KPSI
 	const _rev4 = 0.5 - deltaTh * KTH - deltaPsi * KPSI
 	
-	ctrl.rev1 = Math.min(1, Math.max(0, _rev1))
-	ctrl.rev2 = Math.min(1, Math.max(0, _rev2))
-	ctrl.rev3 = Math.min(1, Math.max(0, _rev3))
-	ctrl.rev4 = Math.min(1, Math.max(0, _rev4))
+	ctrl.rev1 = Math.min(1, Math.max(0, _rev1)) * Math.max(0, 1 - 5.85*Th)
+	ctrl.rev2 = Math.min(1, Math.max(0, _rev2))* Math.max(0, 1 - 5.85*Th)
+	ctrl.rev3 = Math.min(1, Math.max(0, _rev3))* Math.max(0, 1 - 5.85*Th)
+	ctrl.rev4 = Math.min(1, Math.max(0, _rev4))* Math.max(0, 1 - 5.85*Th)
+	console.log(ctrl)
 }
 
 function derivatives(state, ctrl, prms) {
@@ -62,17 +64,14 @@ function derivatives(state, ctrl, prms) {
 	const STH = Math.sin(Th)
 	const CPSI = Math.cos(Psi)
 	const SPSI = Math.sin(Psi)
-	const dir = alpha > 0 ? -1 : 1
 	const XA = Cxa * Q * prms.S
 	const YA = Cya * Q * prms.S
 	const G = prms.m * g
 	const MV = prms.m * V
 	
-	const AX = (R * CA  * dir - XA - G * STH) / prms.m
- 	const dTH = (R * SA * CG + YA * CG - G * CTH) / MV
-    const dPSI = (R * SA * SG + YA * SG) / MV
-	
-    console.log(Q, CA, R, dir, G)
+	const AX = (-R * SA - XA - G * STH) / prms.m
+ 	const dTH = (R * CA * CG + YA * CG - G * CTH) / MV
+    const dPSI = (R * CA * SG + YA * SG) / MV
 
 	const dAlpha = prms.w * (ctrl.rev1 + ctrl.rev2 - ctrl.rev3 - ctrl.rev4) * prms.lx/prms.Jz
 	const dGamma = prms.w * (ctrl.rev1 + ctrl.rev3 - ctrl.rev2 - ctrl.rev4) * prms.ly/prms.Jx
@@ -88,7 +87,11 @@ function derivatives(state, ctrl, prms) {
 	  wGamma,
 	  V * CTH * CPSI,
 	  V * STH,
-	  V * CTH * SPSI
+	  V * CTH * SPSI,
+	  0,
+	  0,
+	  0,
+	  0,
 	]
 }
 
@@ -96,26 +99,33 @@ function derivatives(state, ctrl, prms) {
 function integrate(state, prms, prog, dT, tMax) {
 	const dT_05 = dT * 0.5
 	const currentControls = {
-		rev1: 0.45,
-		rev2: 0.45,
-		rev3: 0.55,
-		rev4: 0.55,
+		rev1: 0.4,
+		rev2: 0.4,
+		rev3: 0.6,
+		rev4: 0.6,
 	}
 	let t = 0
+	let i = 0
 	const result = [state.slice()]
 
 	while(t < tMax) {
-		setControls(state, currentControls, prog)
+		const _state = result[i].slice() 
+		setControls(_state, currentControls, prog)
 
 		const K1 = derivatives(state, currentControls, prms)
-		const state_K1 = arrayCombination(state, K1, 1, dT)
+		const state_K1 = arrayCombination(_state, K1, 1, dT)
 		const K2 = derivatives(state_K1, currentControls, prms)
 		const derivSumm = arraySumm(K1, K2)
 		
-		const state2 = arrayCombination(state, derivSumm, 1, dT_05)
+		const state2 = arrayCombination(_state, derivSumm, 1, dT_05)
 		t += dT
 		state2[0] = t
+		state2[11] = currentControls.rev1
+		state2[12] = currentControls.rev2
+		state2[13] = currentControls.rev3
+		state2[14] = currentControls.rev4
 		result.push(state2)
+		i++
 	}
 
 	return result
@@ -145,7 +155,7 @@ function arrayCombination(U, V, kU, kV) {
 {
     const dT = 0.01
 	                  //t V  Th Psi wA wG  AoA G  X  Y  Z
-	const testState = [0, 5, 0, 0,  0, 0,  0,  0, 0, 0, 0]
+	const testState = [0, 5, 0, 0,  0, 0,  0,  0, 0, 500, 0, 0.5, 0.5, 0.5, 0.5]
 	const testParams = {
 		m: 2.5,
 		S: 0.032,
@@ -158,16 +168,16 @@ function arrayCombination(U, V, kU, kV) {
 
 	const testProg = {
 		getTh: function(t) {
-			return 0
+			return -(17.5 / 57.3)
 		},
 		getPsi: function(t) {
 			return 0
 		}
 	}
-	const res = integrate(testState, testParams, testProg, dT, 1.5)
+	const res = integrate(testState, testParams, testProg, dT, 45.5)
 	let output = ''
 	res.forEach(r => {
-		const [t, V,  Th, Psi, wA, wG,  AoA, G,  X,  Y,  Z] = r
+		const [t, V,  Th, Psi, wA, wG,  AoA, G,  X,  Y,  Z, r1, r2, r3, r4] = r
 		const str = [
 			t.toFixed(2),
 			V.toFixed(1),
@@ -175,12 +185,15 @@ function arrayCombination(U, V, kU, kV) {
 			(Psi * 57.3).toFixed(2),
 			(wA * 57.3).toFixed(2),
 			(wG * 57.3).toFixed(2),
-			(Psi * 57.3).toFixed(2),
 			(AoA * 57.3).toFixed(2),
 			(G * 57.3).toFixed(2),
 			X.toFixed(1),
 			Y.toFixed(1),
 			Z.toFixed(1),
+			r1.toFixed(2),
+			r2.toFixed(2),
+			r3.toFixed(2),
+			r4.toFixed(2),
 		].join('\t')
 
 		output += (str + '\n')
